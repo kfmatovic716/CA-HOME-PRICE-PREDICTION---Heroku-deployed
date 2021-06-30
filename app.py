c68e97de9aa6c522249dbab3712308840a964b46
Katherine Matovic <jabuka611@gmail.com>
2021-06-30 02:05:43 -0700

    Update html files and remove comments

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ import os
 
 # Uncomment this line if running python app.py server
 # Enter password if using this engine
-# engine = create_engine("postgresql://postgres:<password>@localhost:5432/ca_homeprice_db")
+engine = create_engine("postgresql://postgres:Sofija53!@localhost:5432/ca_homeprice_db")
 
 # Use this for Heroku. Uncomment line 13 when using this code
-engine = create_engine(os.environ.get('DATABASE_URL', ''))
+# engine = create_engine(os.environ.get('DATABASE_URL', ''))
 
 # Instantiate a Flask app
 app = Flask(__name__)
@@ -56,7 +56,6 @@ def team():
 @app.route("/documentation")
 def documentation():
     return render_template('documentation.html')
-
 
 # Run your app
 
